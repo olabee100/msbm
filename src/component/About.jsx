dfc9df2c4fc0a9ad48ba721f928444f162e539c8
olabee100 <olabayoantonio@gmail.com>
2024-03-26 23:03:09 +0100

    cool

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,65 +1,63 @@
-import React from 'react'
-import icon1 from "../images/icon1.png"
-import icon2 from "../images/icon2.png"
-import icon3 from "../images/icon3.png"
-import icon4 from "../images/icon4.png"
+import React from 'react';
+import icon1 from "../images/icon1.png";
+import icon2 from "../images/icon2.png";
+import icon3 from "../images/icon3.png";
+import icon4 from "../images/icon4.png";
+import './About.css'; 
 
 const About = () => {
   return (
     <div>
-        <div class="container-xxl py-5">
-        <div class="container">
-            <div class="row g-2 text-center">
-                <div class="col-12 wow fadeInUp" data-wow-delay="0.3s">
-                    <h1 style= {{color: "#fb873f"}}>Invest in your professional goals with MSBM</h1>
-                    <p class="mb-5">Get unlimited access to over 90% of courses, Projects, Specializations, and
-                        Professional Certificates on Coursera, taught by top instructors.</p>
-                </div>
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-2 text-center">
+            <div className="col-12 wow fadeInUp" data-wow-delay="0.3s">
+              <h1 style={{ color: "#fb873f" }}>Invest in your professional goals with MSBM</h1>
+              <p className="mb-5">Get unlimited access to over 90% of courses, Projects, Specializations, and Professional Certificates on Coursera, taught by top instructors.</p>
             </div>
-            <div class="row g-4">
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="service-item text-center pt-3 shadow">
-                        <div class="p-4">
-                            <img src={icon1}alt="" width="60px" class="mb-4" />
-                            <h5 class="mb-3">Learn anything</h5>
-                            <p>Explore any interest or trending topic, take prerequisites, and advance your skills</p>
-                        </div>
-                    </div>
+          </div>
+          <div className="row g-4">
+            <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+              <div className="service-item text-center pt-3 shadow">
+                <div className="p-4">
+                  <img src={icon1} alt="" width="60px" className="mb-4" />
+                  <h5 className="mb-3">Learn anything</h5>
+                  <p>Explore any interest or trending topic, take prerequisites, and advance your skills</p>
                 </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="service-item text-center pt-3 shadow">
-                        <div class="p-4">
-                            <img src={icon2} alt="" width="60px" class="mb-4" />
-                            <h5 class="mb-3">Save money</h5>
-                            <p>Spend less money on your learning if you plan to take multiple courses this year</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="service-item text-center pt-3 shadow">
-                        <div class="p-4">
-                            <img src={icon3} alt="" width="60px" class="mb-4" />
-                            <h5 class="mb-3">Flexible learning</h5>
-                            <p>Learn at your own pace, move between multiple courses, or switch to a different course
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                    <div class="service-item text-center pt-3 shadow">
-                        <div class="p-4">
-                            <img src={icon4} alt="" width="60px" class="mb-4" />
-                            <h5 class="mb-3">Unlimited certificates</h5>
-                            <p>Earn a certificate for every learning program that you complete at no additional cost</p>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
+            <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+              <div className="service-item text-center pt-3 shadow">
+                <div className="p-4">
+                  <img src={icon2} alt="" width="60px" className="mb-4" />
+                  <h5 className="mb-3">Save money</h5>
+                  <p>Spend less money on your learning if you plan to take multiple courses this year</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+              <div className="service-item text-center pt-3 shadow">
+                <div className="p-4">
+                  <img src={icon3} alt="" width="60px" className="mb-4" />
+                  <h5 className="mb-3">Flexible learning</h5>
+                  <p>Learn at your own pace, move between multiple courses, or switch to a different course</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+              <div className="service-item text-center pt-3 shadow">
+                <div className="p-4">
+                  <img src={icon4} alt="" width="60px" className="mb-4" />
+                  <h5 className="mb-3">Unlimited certificates</h5>
+                  <p>Earn a certificate for every learning program that you complete at no additional cost</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-   
-    </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import OwlCarousel from 'react-owl-carousel2';
-import 'react-owl-carousel2/lib/styles.css'; 
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import carousel1 from "../images/carousel1.jpg";
 import carousel2 from "../images/carousel2.jpg";
-import './Hero.css'; 
-
-const options = {
-  items: 1,
-  loop: true,
-  nav: true, 
-  navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'], // Define custom icons for navigation
-  dots: false,
-  autoplay: true,
-};
+import './Hero.css';
 
 const Hero = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 900,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: true,
+   
+  };
+
   return (
     <div className="container-fluid p-0 h-[40px] mb-4">
-      <OwlCarousel options={options}>
-        <div className="owl-carousel-item  position-relative">
+      <Slider {...settings}>
+        <div className="position-relative">
           <img className="img-fluid" src={carousel1} alt="" />
           <div className="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style={{ background: "rgba(24, 29, 56, .7)" }}>
             <div className="container">
@@ -34,7 +37,7 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="owl-carousel-item position-relative">
+        <div className="position-relative">
           <img className="img-fluid" src={carousel2} alt="" />
           <div className="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style={{ background: "rgba(63, 66, 87, 0.7)" }}>
             <div className="container">
@@ -43,14 +46,14 @@ const Hero = () => {
                   <h5 className="text-uppercase mb-3 animated slideInDown" style={{ color: "#fb873f", fontSize: "3vw", maxWidth: "100%" }}>Welcome to Metropolitan School of Business and Management</h5>
                   <h1 className="display-3 text-white animated slideInDown" style={{ fontSize: "4vw", maxWidth: "100%" }}>Interactive Learning Experience</h1>
                   <p className="text-white mb-4 pb-2" style={{ fontSize: "2vw", maxWidth: "100%" }}>Engage with interactive lessons, quizzes, and projects. Experience hands-on learning that keeps you motivated and inspired.</p>
-                  <a href="/" style={{ backgroundColor: "#fb873f" }} className="btn  py-md-2 px-md-4 me-3 animated slideInLeft">Read More</a>
-                  <a href="/" className="btn btn-light py-md-2 px-md-4 animated slideInRight">Join Now</a>
+                  <a href="/" style={{ backgroundColor: "#fb873f" }} className="btn  py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
+                  <a href="/" className="btn btn-light py-md-3 px-md-5 animated slideInRight">Join Now</a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </OwlCarousel>
+      </Slider>
     </div>
   );
 };
